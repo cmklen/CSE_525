d886a956442f98a92041932f1ffbf318dc7c0a28
cmklen <cmklen01@louisville.edu>
2021-09-29 13:30:03 -0400

    fix

--- src/lesson01/src/mini_uart.c
+++ src/lesson01/src/mini_uart.c
@@ -2,8 +2,8 @@
 #include "peripherals/mini_uart.h"
 #include "peripherals/gpio.h"
 
-#define clk_frq 250000000
-#define baud_rate_reference 9600
+#define CLK_FRQ 250000000
+#define BAUD_RATE 9600
 
 void uart_send ( char c )
 {
@@ -33,9 +33,7 @@ void uart_send_string(char* str)
 void uart_init ( void )
 {
 	unsigned int selector;
-	unsigned int baud_rate;
-
-	unsigned int calculated_baud_rate = (clk_frq / (8*baud_rate_reference)) - 1;
+	unsigned int baud_rate = (CLK_FRQ / (8*BAUD_RATE)) - 1;
 
 	selector = get32(GPFSEL1);
 	selector &= ~(7<<12);                   // clean gpio14
@@ -55,7 +53,7 @@ void uart_init ( void )
 	put32(AUX_MU_IER_REG,0);                //Disable receive and transmit interrupts
 	put32(AUX_MU_LCR_REG,3);                //Enable 8 bit mode
 	put32(AUX_MU_MCR_REG,0);                //Set RTS line to be always high
-	put32(AUX_MU_BAUD_REG, calculated_baud_rate);             //Set baud rate to 115200
+	put32(AUX_MU_BAUD_REG, baud_rate);             //Set baud rate to 115200
 
 	put32(AUX_MU_CNTL_REG,3);               //Finally, enable transmitter and receiver
 }
