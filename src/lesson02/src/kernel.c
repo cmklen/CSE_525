f0a3f6114da95357e0ae99a813e434896b2a3278
cmklen <cmklen01@louisville.edu>
2021-10-01 13:04:04 -0400

    lesson2-exercise1

--- src/lesson02/src/kernel.c
+++ src/lesson02/src/kernel.c
@@ -2,12 +2,16 @@
 #include "utils.h"
 #include "mini_uart.h"
 
-void kernel_main(void)
+void kernel_main_el2(void)
 {
 	uart_init();
 	init_printf(0, putc);
-	int el = get_el();
-	printf("Exception level: %d \r\n", el);
+	printf("Exception level: %d \r\n", get_el());
+}
+
+void kernel_main_el1(void)
+{
+	printf("Exception level: %d \r\n", get_el());
 
 	while (1) {
 		uart_send(uart_recv());
